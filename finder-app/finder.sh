2d310d828afb2d44f69bbe7a1c535ee512c53f1d
perry <perry910424@gmail.com>
2023-02-11 15:18:09 +0000

    very good

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
+#!/usr/bin/bash
 filedir=$1
 searchstr=$2
 if [ $# -lt 2 ]
 then
 	exit 1
 fi
-if [ ! -d $filedir ]
+if [ -d $filedir ]
 then
-	exit 1
+	X=`find $filedir -type f | wc -l`
+        Y=`grep -r $filedir -e $searchstr | wc -l`
+        echo "The number of files are $X and the number of matching lines are $Y"
 else
-	X=`find $filedir -type f | wc -l` 
-	Y=`find $filedir -type f -name "$searchstr" | wc -l` 
-	echo The number of files are $X and the number of matching lines are $Y
+	echo no directory
+	exit 1
 fi
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,12 +1,15 @@
+#!/usr/bin/bash
 writefile=$1
 writestr=$2
 if [ $# -lt 2 ]
 then
 	exit 1
 fi
+mkdir -p ${writefile%/*}
 if touch $writefile 2> /dev/null
 then
-	cat $writestr > $writefile
+	echo $writestr > $writefile
 else
+	echo f**k
 	exit 1
 fi
